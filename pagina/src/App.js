85eae86c2f627dc09fec131fadf4034933070e7c
Parpot <56718051+Parpot@users.noreply.github.com>
2020-07-01 18:58:10 +0100

    PostChange
    Adicionei method post para enviar os dados(falta conseguir mandar imagem).
    No fetch mudar o link para a outra aplicação

--- pagina/src/App.js
+++ pagina/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <button className="Button">Press me!</button>
-    </div>
-  );
-}
+class App  extends Component{
+
+  async postData(){
+      try{
+          let result= await fetch('https://webhook.site/60821a8a-fe1b-4c64-849a-6d982b94e6a9', {
+            method: 'post',
+            mode: 'no-cors',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                key1: 'myusername'
+            })
+          });
+          console.log('result' + result)
+      }catch(e){
+        console.log(e)
+      }
+  }
+  render() {
+
+    return (
+      <div className="App">
+        <button onClick={() => this.postData() } className="Button">Press me!</button>
+      </div>
+    );
+  }
+} 
 
 export default App;
