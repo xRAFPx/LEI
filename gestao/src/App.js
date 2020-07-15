e61f4c8afc167da95851e516c32529e77bfc38c9
Carlos Monteiro <a13260@alunos.ipca.pt>
2020-07-15 18:02:34 +0100

    Atualização do display
    O html está organizado e à espera de entrada de dados.

--- gestao/src/App.js
+++ gestao/src/App.js
@@ -1,64 +1,98 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 import './App.css';
-import Col from 'react-bootstrap/Col';
-
+import screenshot from './Screenshot.jpg';
+import header from './header.png';
+import {Container} from 'react-bootstrap'
+import {Form} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
+import {Row} from 'react-bootstrap'
+import {InputGroup} from 'react-bootstrap'
+import {FormControl} from 'react-bootstrap'
+import {Image} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
+ 
 function App() {
+
+  const onChangeHandler=event=>{
+
+    console.log(event.target.files)
+
+  }
+
   return (
-    <div className="App">
-      <Form>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Group controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
+    <Container>
+      <Row>
+        <Col style={{height: '150px'}}>
+          <Image style={{width: '100%'}} src={header} fluid/>
+        </Col>
+      </Row>
+      <Row>
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Control as="select" defaultValue="Tipo de pedido">
+            <option disabled>Tipo de pedido</option>
+            <option>1</option>
+          </Form.Control>
         </Form.Group>
-
-        <Form.Group controlId="formGridAddress2">
-          <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Control as="select" defaultValue="Natureza de pedido">
+            <option disabled>Natureza de pedido</option>
+            <option>1</option>
+          </Form.Control>
         </Form.Group>
-
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Control as="select" defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Control>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Group id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+      </Row>
+      <Row>
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Control as="select" defaultValue="Serviço">
+            <option disabled>Serviço</option>
+            <option>1</option>
+          </Form.Control>
         </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </div>
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Control as="select" defaultValue="Requisitante">
+            <option disabled>Requisitante</option>
+            <option>1</option>
+          </Form.Control>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Control as="select" defaultValue="Email">
+            <option disabled>Email</option>
+            <option>1</option>
+          </Form.Control>
+        </Form.Group>
+        <InputGroup as={Col} className="mb-3">
+          <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Contacto"/>
+        </InputGroup>
+      </Row>
+      <InputGroup className="mb-3">
+        <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Erro"/>
+      </InputGroup>
+      <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Descrição</Form.Label>
+        <Form.Control as="textarea" rows="6" />
+      </Form.Group>
+      <Form.Group controlId="formGridState">
+        <Form.Control as="select" defaultValue="Prioridade">
+          <option disabled>Prioridade</option>
+          <option>1</option>
+        </Form.Control>
+      </Form.Group>
+      <Form.Label>Screenshot</Form.Label>
+      <Row>
+       <Col md={{ span: 8, offset: 2 }}>
+         <Image src={screenshot} fluid/>
+       </Col>
+      </Row>
+      <p/>
+      <form method="post" action="#" id="#">
+        <div class="form-group files">
+          <label>Upload Your File </label>
+          <input type="file" name="file" multiple onChange={onChangeHandler}/>
+        </div>   
+      </form>
+      <Button className="float-right" variant="outline-primary">Submeter</Button>{' '}
+    </Container>
   );
 }
 
