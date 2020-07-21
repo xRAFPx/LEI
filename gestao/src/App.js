d9f6e31f4b0d11a19450399a04bac26fce9bcf68
Carlos Monteiro <a13260@alunos.ipca.pt>
2020-07-21 18:47:41 +0100

    Projeto teste para testes
    Um projeto apra testar codigo sem estragar nada

--- gestao/src/App.js
+++ gestao/src/App.js
@@ -20,79 +20,79 @@ function App() {
   }
 
   return (
-    <Container>
-      <Row>
-        <Col style={{height: '150px'}}>
-          <Image style={{width: '100%'}} src={header} fluid/>
-        </Col>
-      </Row>
-      <Row>
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Control as="select" defaultValue="Tipo de pedido">
-            <option disabled>Tipo de pedido</option>
-            <option>1</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Control as="select" defaultValue="Natureza de pedido">
-            <option disabled>Natureza de pedido</option>
-            <option>1</option>
-          </Form.Control>
-        </Form.Group>
-      </Row>
-      <Row>
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Control as="select" defaultValue="Serviço">
-            <option disabled>Serviço</option>
-            <option>1</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Control as="select" defaultValue="Requisitante">
-            <option disabled>Requisitante</option>
-            <option>1</option>
-          </Form.Control>
-        </Form.Group>
-      </Row>
-      <Row>
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Control as="select" defaultValue="Email">
-            <option disabled>Email</option>
-            <option>1</option>
-          </Form.Control>
-        </Form.Group>
-        <InputGroup as={Col} className="mb-3">
-          <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Contacto"/>
+      <Container>
+        <Row>
+          <Col style={{height: '150px'}}>
+            <Image style={{width: '100%'}} src={header} fluid/>
+          </Col>
+        </Row>
+        <Row>
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Control as="select" defaultValue="Tipo de pedido">
+              <option disabled>Tipo de pedido</option>
+              <option>1</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Control as="select" defaultValue="Natureza de pedido">
+              <option disabled>Natureza de pedido</option>
+              <option>1</option>
+            </Form.Control>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Control as="select" defaultValue="Serviço">
+              <option disabled>Serviço</option>
+              <option>1</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Control as="select" defaultValue="Requisitante">
+              <option disabled>Requisitante</option>
+              <option>1</option>
+            </Form.Control>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Control as="select" defaultValue="Email">
+              <option disabled>Email</option>
+              <option>1</option>
+            </Form.Control>
+          </Form.Group>
+          <InputGroup as={Col} className="mb-3">
+            <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Contacto"/>
+          </InputGroup>
+        </Row>
+        <InputGroup className="mb-3">
+          <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Erro"/>
         </InputGroup>
-      </Row>
-      <InputGroup className="mb-3">
-        <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Erro"/>
-      </InputGroup>
-      <Form.Group controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Descrição</Form.Label>
-        <Form.Control as="textarea" rows="6" />
-      </Form.Group>
-      <Form.Group controlId="formGridState">
-        <Form.Control as="select" defaultValue="Prioridade">
-          <option disabled>Prioridade</option>
-          <option>1</option>
-        </Form.Control>
-      </Form.Group>
-      <Form.Label>Screenshot</Form.Label>
-      <Row>
-       <Col md={{ span: 8, offset: 2 }}>
-         <Image src={screenshot} fluid/>
-       </Col>
-      </Row>
-      <p/>
-      <form method="post" action="#" id="#">
-        <div class="form-group files">
-          <label>Upload Your File </label>
-          <input type="file" name="file" multiple onChange={onChangeHandler}/>
-        </div>   
-      </form>
-      <Button className="float-right" variant="outline-primary">Submeter</Button>{' '}
-    </Container>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Descrição</Form.Label>
+          <Form.Control as="textarea" rows="6" />
+        </Form.Group>
+        <Form.Group controlId="formGridState">
+          <Form.Control as="select" defaultValue="Prioridade">
+            <option disabled>Prioridade</option>
+            <option>1</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Label>Screenshot</Form.Label>
+        <Row>
+        <Col md={{ span: 8, offset: 2 }}>
+          <Image src={screenshot} fluid/>
+        </Col>
+        </Row>
+        <p/>
+        <form method="post" action="#" id="#">
+          <div class="form-group files">
+            <label>Upload Your File </label>
+            <input type="file" name="file" multiple onChange={onChangeHandler}/>
+          </div>   
+        </form>
+        <Button type="submit" className="float-right" variant="outline-primary">Submeter</Button>{' '}
+      </Container>
   );
 }
 
